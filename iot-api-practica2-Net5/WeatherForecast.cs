1f80ba2865400624eca3b3470ba5f0dfeff9d17a
jrfito <jrfito@hotmail.com>
2021-10-29 21:49:26 -0500

    Initial commit

---
+++ iot-api-practica2-Net5/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace iot_api_practica2_Net5
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
